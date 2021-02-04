5f721c723ad85759a8c54da4366ac119fc0d3ebd
root <qhdgkdbs@gmail.com>
2021-02-04 06:21:56 +0000

    saveReqRouter

--- index.js
+++ index.js
@@ -9,16 +9,42 @@ app.use(logger('dev', {}));
 app.use(bodyParser.json());
 app.use('/api', apiRouter);
 
-apiRouter.post('/sayHello', function(req, res) {
+var arr_req = []
+
+apiRouter.post('/getMyTracking', function(req, res) {
+	console.log(req.body.action.params.city)
     const responseBody = {
       version: "2.0",
       template: {
         outputs: [
           {
             simpleText: {
-              text: "hello I'm Ryan"
+              text: req.body.action.params.city + "에 사시는 군요" 
             }
           }
+        ]
+      }
+    };
+  
+    res.status(200).send(responseBody);
+  });
+
+apiRouter.post('/saveReq', function(req, res) {
+	var userReq = req.body.action.params.questions
+	var userName = req.body.action.params.name
+	var userNumber = req.body.action.params.number
+	
+	console.log(req.body)
+    const responseBody = {
+      version: "2.0",
+      template: {
+        outputs: [
+			{
+				simpleText: {text : "성함 : " + userName +"\n연락처 : "+userNumber +"\n문의내용 : "+ userReq}
+			},
+			{
+				simpleText: {text : "위의 내용으로 문의가 저장되었습니다. \n밀레 코리아를 이용해주셔서 감사합니다."}
+		  	}
         ]
       }
     };
