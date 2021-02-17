e298bd4930b6db3c10d92422c88eef8c1a1218fb
qhdgkdbs <qhdgkdbs@gmail.com>
2021-02-17 18:04:00 +0900

    add ignore

--- config.js
+++
@@ -1,1 +0,0 @@
-export const MOGNODB_URL = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI : 'mongodb://localhost:27017/miele_tools_on_web?gssapiServiceName=mongodb'
-// export const MOGNODB_URL = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
-}
