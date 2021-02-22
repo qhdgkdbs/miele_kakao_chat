4c650bc94d0054c0821e150cab1b21ff83c1dfa4
qhdgkdbs <qhdgkdbs@gmail.com>
2021-02-22 16:33:39 +0900

    update

--- models/userReq.js
+++ models/userReq.js
@@ -8,13 +8,28 @@ function formatDate(date) {
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
-    return date.getFullYear()+ "/" + (date.getMonth()+1) + "/" + date.getDate();
+    var month = (date.getMonth()+1) ? '0'+(date.getMonth()+1) : (date.getMonth()+1);
+    return date.getFullYear()+ "" + month + "" + date.getDate();
 } 
+
+function formatTime(date){
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + '' + minutes
+
+    return strTime
+}
 
 
 function getDate(){
     var d = new Date();
     return formatDate(d);
+}
+
+function getTime(){
+    var d = new Date();
+    return formatTime(d)   
 }
 
 const userReqSchema = mongoose.Schema({
@@ -41,6 +56,10 @@ const userReqSchema = mongoose.Schema({
     reqCreated: {
         type: String,
         default: getDate()
+    },
+    reqCreatedTime: {
+        type: String,
+        default: getTime()
     },
     mieleMemo : {
         type: String
--- router/getDB.js
+++ router/getDB.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const { Tracking } = require("../models/Tracking"); 
+const { UserReq } = require("../models/userReq"); 
+
 
 router.get('/' , (req, res, next) => {
 
-    Tracking.find({ name:"곽경진"}, (err, data) => {
+    UserReq.find({ 
+        reqCreated : { $gte :  '20210219', $lte: '20210222' },
+        name : "봉승우",
+        call : "010",
+        mieleRes : { $exists: true },
+        // mieleMemo : { $exists: false },
+    }, (err, data) => {
         if(err) return res.json(err)
 
         if (data) {
